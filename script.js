fab975ef0655920a2de7ac01bbf7cfcfbb93460d
MarinaSobhy <74794948+MarinaSobhy@users.noreply.github.com>
2021-11-04 14:06:51 +0200

    edit

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ $(window).scroll(function(){
     let heroHeight=$('#hero').height()
     let top = $(window).scrollTop();
     if(top>heroHeight){
-        console.log('white')
         $('nav').removeClass('bg-transparent navbar-dark');
         $('nav').addClass('bg-white navbar-light shadow-sm')
     }
         
     else{
-        console.log('trans')
         $('nav').addClass('bg-transparent navbar-dark')
         $('nav').removeClass('bg-white navbar-light shadow-sm');
     }
